a63d46dcabd17bc1310669caa3d7cfb66fba09ab
연재환 <woghks1213@gmail.com>
2024-07-31 11:53:15 +0900

    update swea 5648 원자 소멸 시뮬레이션

--- day_6/practice/swea_5648.py
+++ day_6/practice/swea_5648.py
@@ -1,9 +1,10 @@
-# [모의 SW 역량테스트] 원자 소멸 시뮬레이션
-# https://swexpertacademy.com/main/solvingProblem/solvingProblem.do
-
 import sys
 
 sys.stdin = open("../inputs/input_5648.txt")
+
+from collections import defaultdict
+
+T = int(input())
 
 directions = {
     0: [0, 1],  # 상
@@ -11,88 +12,82 @@ directions = {
     2: [-1, 0],  # 좌
     3: [1, 0]  # 우
 }
-"""
-두 원자가 충돌하는 경우 
-    두 원자가 직선상에서 만나는 경우 
-        상 - 하 
-        좌 - 우 
-    두 원자가 수직으로 만나는 경우 
-        상 - 우
-        상 - 좌 
-        하 - 우 
-        하 - 좌 
-세 원자가 충돌하는 경우 
-    상 - 우 - 하 
-    상 - 우 - 좌
-"""
-
-"""
-원자들의 수 N은 1000개 이하이다.
-
-원자들의 위치는 -1000 이상 1000이하의 정수로 주어진다.
-원자들이 움직일 수 있는 좌표의 범위에 제한은 없다.
-
-현재 원자를 기준으로 충돌 가능성이 있는 원자들을 후보 충돌 원자로 설정.
-"""
 
 
-def check_extinction(atom1, atom2):
+def remove_elements(original_list, remove_index):
     """
-    :param atom1: 원자 1의 정보 (x, y ,d, k)
-    :param atom2: 원자 2의 정보 (x, y, d, k)
-    :return: boolean (두 원자가 충돌 가능성이 있는 경우 True, 충돌 가능성이 없다면 False)
+    remove_elements() 함수는 original_list에서 remove_index 내 인덱스에 해당하는 원소를 삭제하는 함수
+    :param original_list: 원본 리스트
+    :param remove_index: 삭제할 원소 인덱스
+    :return: 원소들을 제거한 리스트
     """
-    atom1_x, atom1_y, atom1_d, _ = atom1
-    atom2_x, atom2_y, atom2_d, _ = atom2
+    # 인덱스를 내림차순으로 정렬
+    remove_index.sort(reverse=True)
 
-    # 두 원자가 평행하게 움직이는 경우 (서로 다른 열 -> 좌/우로 움직임)
-    if atom1_y != atom2_y and atom1_d >= 2 and atom2_d >= 2:
-        return False
-        # 두 원자가 평행하게 움직이는 경우 2 (서로 다른 행 -> 위/아래로 움직임)
-    if atom1_x != atom2_x and atom1_d <= 1 and atom2_d <= 1:
-        return False
-        # 두 원자가 같은 y좌표에서 에서 같은 방향으로 움직이는 경우
-    if atom1_y == atom2_y and atom1_d == atom2_d:
-        return False
-    # 두 원자가 같은 x좌표에서 같은 방향으로 움직이는 경우
-    if atom1_x == atom2_x and atom1_d == atom2_d:
-        return False
+    # 새로운 리스트 생성
+    result = original_list.copy()
 
-    # 충돌 가능성이 있는 경우
-    return True
+    # 정렬된 인덱스를 순회하며 요소 제거
+    for index in remove_index:
+        if 0 <= index < len(result):
+            result = result[:index] + result[index + 1:]
 
+    return result
 
-def delete_element(lst:list, idx:int) -> list :
-    lst[idx] = lst[-1]
-    lst.pop()
-    return lst
-
-
-def atom_extinction(atoms):
-    total_energy = 0
-    while atoms:
-        atom = atoms.pop()
-        extinction_candidate = []  # 현재 원자와 충돌 가능성이 있는 후보 원자 리스트
-        for i in range(len(atoms)):
-            if atoms[i][0] == atom[0] and atoms[i][1] == atom[1]:
-                continue
-            atom2 = atoms[i]
-            if check_extinction(atom1=atom, atom2=atom2):
-                extinction_candidate.append(atom2)
-
-        # 충돌 가능성이 있는 후보들 중 가장 먼저 충돌돌
-
-    retur total_energy
-
-
-T = int(input())  # 테스트 케이스 개수
 
 for tc in range(1, T + 1):
-    N = int(input())  # 원자 수
+    N = int(input())  # 원자 개수
+    # atoms 원소 구조 : [x: 초기 x 좌표, y: 초기 y 좌표, d: 이동 방향, k: 보유 에너지]
+    atoms = [list(map(int, input().split())) for _ in range(N)]
 
-    # ((x:x좌표, y:y좌표), d:이동방향, k:보유 에너지 )
-    atoms = [list(map(int, input().rstrip().split())) for _ in range(N)]
+    total_energy = 0
+    # 충돌이 일어나지 않는다는 가정하에 예상 이동 시간(거리)가 가장 긴 원소의 이동시간 : max_time
+    max_time = 0
 
-    result = atom_extinction(atoms)
+    for atom in atoms:
+        direction = atom[2]
+        if direction == 0:  # 상
+            tmp_time = 1000 - atom[1]
+            max_time = max(max_time, tmp_time)
+        elif direction == 1:  # 하
+            tmp_time = -1000 - atom[1]
+            max_time = max(max_time, abs(tmp_time))
+        elif direction == 2:  # 좌
+            tmp_time = 1000 - atom[0]
+            max_time = max(max_time, tmp_time)
+        elif direction == 3:  # 우
+            tmp_time = -1000 - atom[0]
+            max_time = max(max_time, abs(tmp_time))
+        else:
+            raise ValueError("원자의 방향 정보가 올바르지 않습니다.")
 
-    print(f"#{tc} {result}")
+    step = 0.5
+    time = 0.5
+    # 최대 이동 시간동안 반복
+    while time <= max_time:
+        # 남은 원자가 존재하지 않는다면 while 문 종료
+        if not atoms:
+            break
+
+        # 0.5초 마다 위치 갱신
+        next_position_dict = defaultdict(list)  # 위치 정보 담을 딕셔너리
+        for i in range(len(atoms)):  # 모든 원자에 대해 순회
+            atom = atoms[i]  # 원자
+            x, y, d = atom[0], atom[1], atom[2]  # 원자 좌표와 이동 방향(0 or 1 or 2 or 3)
+            dx, dy = directions[d]  # x 및 y 방향에 대한 이동 방향
+            nx, ny = x + dx * step, y + dy * step  # 원자의 다음 좌표
+            atoms[i][0], atoms[i][1] = nx, ny  # 원자의 좌표 갱신
+
+            next_position_dict[(nx, ny)].append(i)
+
+        # 충돌한 원자 인덱스
+        remove_index = [idx for indices in next_position_dict.values() if len(indices) > 1 for idx in indices]
+
+        # 충돌한 원자가 있는 경우
+        if remove_index:
+            total_energy += sum(atoms[idx][-1] for idx in remove_index)  # 충돌한 원자의 에너지 방출 (누적 합)
+            atoms = [atom for i, atom in enumerate(atoms) if i not in remove_index]  # 충돌한 원자를 제외한 원자 리스트 갱신
+
+        time += step
+
+    print(f"#{tc} {total_energy}")
