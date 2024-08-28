0c40e039bcf5cec9771d60925012f5cec9251d4c
jaefan <woghks1213@gmail.com>
2024-08-29 08:51:20 +0900

    add: 240829 swea_1868 [파핑파핑 지뢰찾기] solved

--- day_9/swea_1868.py
+++ day_9/swea_1868.py
@@ -1,7 +1,6 @@
 # day_9 homewok
 # 파핑파핑 지뢰찾기
 
-from pprint import pprint
 from collections import deque
 
 def main():
@@ -27,7 +26,6 @@ def main():
                 # 현재 좌표 값을 주변 폭탄 개수로 변경
                 grid[r][c] = bomb_cnt
 
-        pprint(grid)
         # 현재 상황 : 모든 좌표의 값이 8방면 폭탄 개수 혹은 폭탄
         # -> 주변에 폭탄이 없는 좌표(r,c)를 골라서 연쇄적으로 폭탄을 터트린다.
         click_cnt = 0
@@ -51,13 +49,16 @@ def main():
                         visited[nr][nc] = True
                         if grid[nr][nc] != 0: continue
                         queue.append((nr, nc))
-                        # print((nr, nc))
-
-                print(r, c)
                 click_cnt += 1
 
+        # 0으로 연쇄적으로 터트려진 좌표 외에 하나하나 클릭해야 할 좌표들
+        for r in range(N):
+            for c in range(N):
+                if grid[r][c] == 0: continue
+                if grid[r][c] == "*": continue
+                if visited[r][c]: continue
+                click_cnt += 1
         print(f"#{tc} {click_cnt}")
-
 
 
 if __name__ == "__main__":
